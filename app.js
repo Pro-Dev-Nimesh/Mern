96a47bc2283e70bfe086bdce8ac69f1a018877bb
Pro Developer <nimeshsahu39@fmail.com>
2024-12-08 14:22:14 +0530

    app.js file created to the node server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = required('express');
+const app = express();
+const port = 3000;
+
+// Middware top parse JASON requests
+app.use(express.json());
+
+//Root route
+app.get('/', (req, res)=> {
+    res.send('Hello world!');
+});
+
+// API route
+app.get('/api', (req, res)=>{
+    res.jason({message: 'Welcome to the API'});
+});
+
+// Example of a POST rout
+app.post('/api/data',(req, res)=>{
+    const data = req.body;
+    res.jason({message: 'Data recived', data});
+});
+
+app.listen(port, () =>{
+    console.log('Server is running on http://localhost:${port}');
+});
